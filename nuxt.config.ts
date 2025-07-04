b4252feaffacc2aa95c5b136c9a7e857d0148d53
hongrueihuang <ray1682220@gmail.com>
2025-07-04 16:17:46 +0800

    [CI]add baseUrl

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    baseURL: '/demo-stack-modal/'
+  },
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
   modules: ['@nuxt/ui', '@nuxt/test-utils', '@nuxt/eslint'],
